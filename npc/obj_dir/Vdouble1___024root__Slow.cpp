2581b60a8da13e19d7f9c988e975ba2131c067dc
tracer-ysyx <tracer@ysyx.org>
2025-01-10 10:57:03 +0800

    > sim RTL ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:57:03 up  1:54,  1 user,  load average: 0.17, 0.05, 0.04

---
+++ npc/obj_dir/Vdouble1___024root__Slow.cpp
@@ -0,0 +1,25 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vdouble1.h for the primary calling header
+
+#include "verilated.h"
+
+#include "Vdouble1__Syms.h"
+#include "Vdouble1___024root.h"
+
+void Vdouble1___024root___ctor_var_reset(Vdouble1___024root* vlSelf);
+
+Vdouble1___024root::Vdouble1___024root(Vdouble1__Syms* symsp, const char* v__name)
+    : VerilatedModule{v__name}
+    , vlSymsp{symsp}
+ {
+    // Reset structure values
+    Vdouble1___024root___ctor_var_reset(this);
+}
+
+void Vdouble1___024root::__Vconfigure(bool first) {
+    if (false && first) {}  // Prevent unused
+}
+
+Vdouble1___024root::~Vdouble1___024root() {
+}

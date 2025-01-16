c9f9f9d943020e27b1861941d704c17cc136d268
tracer-ysyx <tracer@ysyx.org>
2025-01-16 22:05:03 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:05:03 up  7:21,  1 user,  load average: 0.22, 0.36, 0.61

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -77,7 +77,7 @@ typedef struct token {
   char str[24];
 } Token;
 
-static Token tokens[1024] __attribute__((used)) = {};
+static Token tokens[4096] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {

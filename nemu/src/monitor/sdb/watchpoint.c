310975a628dd8b4ef469173fe51e92ed2c814f45
tracer-ysyx <tracer@ysyx.org>
2025-01-16 14:28:41 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:28:41 up 15 min,  1 user,  load average: 0.12, 0.13, 0.15

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -92,6 +92,8 @@ void create_watchpoint(char* args){
 }
 
 void sdb_watchpoint_display(){
+    int *ptr =NULL;
+    *ptr = 42;
     bool flag = true;
     for(int i = 0 ; i < NR_WP ; i ++){
         if(wp_pool[i].used){

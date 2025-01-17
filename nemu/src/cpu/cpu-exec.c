bc4fd929b1cb2a145630568db50bbfa43cf0d2ed
tracer-ysyx <tracer@ysyx.org>
2025-01-17 15:32:34 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:32:34 up  5:55,  1 user,  load average: 0.37, 0.21, 0.20

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -46,12 +46,12 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 		nemu_state.state = NEMU_STOP;
 	}*/
 	for(int i = 0 ; i < NR_WP; i ++){
-	if(wp_pool[i].count){
+	if(wp_pool[i].flag)
+	{
+	    if(wp_pool[i].count){
 		nemu_state.state = NEMU_STOP;
 		return ;
 	}
-	else if(wp_pool[i].flag)
-	{
 	    bool success = false;
 	    int tmp = expr(wp_pool[i].expr,&success);
 	    if(success){

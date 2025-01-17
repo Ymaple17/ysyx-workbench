fa9849f9b6c68f2f615d55c54d85c50b8dc7e1dd
tracer-ysyx <tracer@ysyx.org>
2025-01-17 18:36:03 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:36:03 up  8:31,  1 user,  load average: 0.22, 0.18, 0.10

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -29,7 +29,7 @@ void isa_reg_display() {
 	    printf("reg%s : 0x%08x\n",regs[i],cpu.gpr[i]);
 }
 }
-/*word_t isa_reg_str2val(const char *s, bool *success) {
+word_t isa_reg_str2val(const char *s, bool *success) {
 	int length=sizeof(regs) / sizeof((regs)[0]);
 	if(strcmp("$pc",s)==0) {
 	   *success=true;
@@ -47,8 +47,8 @@ void isa_reg_display() {
 	}
 	*success = false;
 	return -1;
-}*/
-word_t isa_reg_str2val(const char *s, bool *success) {
+}
+/*word_t isa_reg_str2val(const char *s, bool *success) {
     for(int i = 0 ; i < 32 ; i ++){
    	if(strcmp(regs[i], s) == 0){
 		return cpu.gpr[i];
@@ -56,4 +56,4 @@ word_t isa_reg_str2val(const char *s, bool *success) {
     }
     success = false;
     return 0;
-}
+}*/

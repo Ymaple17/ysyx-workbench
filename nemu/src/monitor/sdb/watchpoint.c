aaa37bfc10416de4fe0c5f43269509bc645b40b7
tracer-ysyx <tracer@ysyx.org>
2025-01-17 15:20:31 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:20:31 up  5:43,  1 user,  load average: 0.14, 0.17, 0.20

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -79,6 +79,7 @@ void free_wp(WP *wp){
     if(head -> NO == wp -> NO){
     	head -> flag = false;
 	head = NULL;
+	head -> count = false;
 	printf("Delete watchpoint  success.\n");
 	return ;
     }
@@ -87,7 +88,7 @@ void free_wp(WP *wp){
 	{
 	    p -> next = p -> next -> next;
 	    p -> next -> flag = false; 
-	    p->count = 0;
+	    p->count = false;
 	    printf("free succes.\n");
 	    return ;
 	}

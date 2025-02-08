a1c973a061378cb30334be85c959d5168b58b31b
tracer-ysyx <tracer@ysyx.org>
2025-02-08 09:05:51 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:05:51 up 18 min,  1 user,  load average: 0.94, 0.39, 0.30

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -229,7 +229,7 @@ static int find_main_operator_index(int p, int q) {//找出主运算符
 	return main_operator_index;
 }
 
-int vaddr_read(vaddr_t, int);
+int paddr_read(paddr_t, int);
 
 int eval(int p, int q, bool *success) {
 	if (p > q) {
@@ -270,7 +270,7 @@ int eval(int p, int q, bool *success) {
 	}
 
 	if (tokens[op].type == TK_DEREF) {
-		return vaddr_read(value_right, 4);
+		return paddr_read(value_right, 4);
 	}
 
 	if (tokens[op].type == TK_NEG) {

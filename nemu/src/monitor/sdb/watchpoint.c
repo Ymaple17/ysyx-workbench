04545ead6784a60c42449901e2ba43aa057c652b
tracer-ysyx <tracer@ysyx.org>
2025-02-16 17:18:52 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:18:52 up 40 min,  1 user,  load average: 0.88, 0.50, 0.41

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -32,6 +32,8 @@ typedef struct watchpoint {
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
 */
+WP* head = NULL;
+WP* free_ = NULL; 
 WP wp_pool[NR_WP] = {};
 void init_wp_pool() {
   int i;
--- nemu/src/monitor/sdb/watchpoint.h
+++ nemu/src/monitor/sdb/watchpoint.h
@@ -1,35 +1,44 @@
 #ifndef WATCHPOINT_H
 #define WATCHPOINT_H
 
-#include "sdb.h"
+#include <stdbool.h>
 
+// 定义最大观察点数
 #define NR_WP 32
 
+// 观察点结构体
 typedef struct watchpoint {
-    int NO;
-    struct watchpoint *next;
-
-    // TODO: Add more members if necessary
-
-    bool flag; // use / unuse
-    char expr[100];
-    int new_value;
-    int old_value;
-    //bool  count;
-
+    int NO;             // 观察点编号
+    struct watchpoint *next; // 链表指针，指向下一个观察点
+    bool flag;          // 标志位，表示该观察点是否被激活
+    char expr[105];     // 表达式，用于存储观察点的条件表达式
+    int new_value;      // 表达式的当前值
+    int old_value;      // 表达式的旧值
 } WP;
-    
-extern WP wp_pool[NR_WP];
-static WP *head __attribute__((unused));
-static WP *free_ __attribute__((unused));
 
-void init_wp_pool();
-WP* new_wp();
+// 声明全局变量
+extern WP wp_pool[NR_WP];   // 观察点池
+extern WP *head;             // 激活的观察点链表头
+extern WP *free_;            // 可用的观察点链表头
+
+// 初始化观察点池
+void init_wp_pool(void);
+
+// 创建一个新的观察点
+WP* new_wp(void);
+
+// 释放一个观察点
 void free_wp(WP *wp);
-void sdb_watchpoint_display();
+
+// 显示所有观察点的状态
+void sdb_watchpoint_display(void);
+
+// 删除指定编号的观察点
 void delete_watchpoint(int no);
+
+// 创建一个观察点，并初始化表达式
 void create_watchpoint(char* args);
-void set_breakpoint(vaddr_t addr);
 
+int expr(char *e, bool *success);
+#endif  // WATCHPOINT_H
 
-#endif

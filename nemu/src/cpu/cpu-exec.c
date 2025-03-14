1a84422341af8e9982fc7a95691d6e418453f749
tracer-ysyx <tracer@ysyx.org>
2025-03-14 17:43:33 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:43:33 up 59 min,  1 user,  load average: 0.88, 0.77, 0.58

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -144,7 +144,7 @@ static void statistic() {
 }
 
 void assert_fail_msg() {
-  display_inst();
+  
   isa_reg_display();
   statistic();
 }
@@ -175,6 +175,10 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
+          if (nemu_state.state == NEMU_ABORT || nemu_state.halt_ret != 0){
+          Log("itrace->mistake:");
+          display_inst();
+         }
       // fall through
     case NEMU_QUIT: statistic();
   }

442eef9f37e10f3e83345790dcf46fd7ee473454
tracer-ysyx <tracer@ysyx.org>
2025-03-29 20:01:29 +0800

    > sim RTL ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:01:29 up  5:42,  1 user,  load average: 0.78, 0.83, 0.72

--- npc/csrc/include/conf.h
+++ npc/csrc/include/conf.h
@@ -7,12 +7,9 @@
 // #define CONFIG_MTRACE 1
 
 #define CONFIG_MAX_WAVETRACE_CLK 32768
-// 设置vcd的flush间隔，1为每次dump都flush一次，0为不flush，n为每n次dump则flush一次
-// 该参数意义在于防止npc意外退出时部分波形数据未及时落盘
 #define CONFIG_WAVETRACE_FLUSH 0
 
-// 添加缺少的宏定义
-#define CONFIG_MSIZE 0x8000000  // 128MB
+#define CONFIG_MSIZE 0x8000000 // 128MB
 #define CONFIG_MBASE 0x80000000 // Base address for physical memory
 #define CONFIG_PC_RESET_OFFSET 0x0 // PC 复位偏移量
 #define RESET_VECTOR (CONFIG_MBASE + CONFIG_PC_RESET_OFFSET) // Reset vector (same as in Top.v)

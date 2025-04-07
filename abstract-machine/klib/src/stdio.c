c03707ffd27dabe6bb1ebe41b5ba2793cdcc961f
tracer-ysyx <tracer@ysyx.org>
2025-04-07 21:49:13 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:49:13 up  5:00,  1 user,  load average: 0.30, 0.42, 0.37

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -34,7 +34,15 @@ int sprintf_string(char *out, const char *str) {
 }
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  char buf[1024];
+  int len = vsprintf(buf, fmt, ap);
+  va_end(ap);
+  for (int i = 0; i < len; i++) {
+    putch(buf[i]);
+  }
+  return len;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -86,12 +94,76 @@ int sprintf(char *out, const char *fmt, ...) {
   return result;
 }
 
+// 实现 snprintf 函数
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int result = vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+  return result;
 }
 
+// 实现 vsnprintf 函数
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  int len = 0;
+  const char *ptr = fmt;
+  while (*ptr && len < n - 1) {
+     if (*ptr == '%' && *(ptr + 1) != '\0') {
+      ptr++;
+      switch (*ptr) {
+        case 'd': {
+          int value = va_arg(ap, int);
+          int num_len = sprintf_number(out + len, value, 10);
+          if (len + num_len < n - 1) {
+            len += num_len;
+          } else {
+            len = n - 1;
+          }
+          break;
+        }
+        case 's': {
+          char *str = va_arg(ap, char*);
+          int str_len = 0;
+          while (str[str_len] && len + str_len < n - 1) {
+            out[len + str_len] = str[str_len];
+            str_len++;
+          }
+          len += str_len;
+          break;
+        }
+        case 'x': {
+          int value = va_arg(ap, int);
+          int num_len = sprintf_number(out + len, value, 16);
+          if (len + num_len < n - 1) {
+            len += num_len;
+          } else {
+            len = n - 1;
+          }
+          break;
+        }
+        case '%': {
+          out[len++] = '%';
+          break;
+        }
+        default: {
+          if (len < n - 2) {
+            out[len++] = '%';
+            out[len++] = *ptr;
+          } else {
+            len = n - 1;
+          }
+          break;
+        }
+      }
+    } else {
+      out[len++] = *ptr;
+    }
+    ptr++;
+  }
+  if (n > 0) {
+    out[len] = '\0';
+  }
+  return len;
 }
 
-#endif
+#endif    

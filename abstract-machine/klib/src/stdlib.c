279b4f2c6919193ce9109b728334eb07697e3363
tracer-ysyx <tracer@ysyx.org>
2025-04-15 14:48:49 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:48:49 up  1:38,  1 user,  load average: 1.15, 0.47, 0.24

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -4,6 +4,7 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
+static uint8_t* p = NULL;
 
 int rand(void) {
   // RAND_MAX assumed to be 32767
@@ -33,6 +34,12 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+  if(p==NULL) p=heap.start;
+  if (p >= (uint8_t*)heap.end) return NULL;
+  void *ret = p;
+  p += size;
+  return ret;
+
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
   panic("Not implemented");
 #endif

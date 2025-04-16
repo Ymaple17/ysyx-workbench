e37fb2bbfb01e5204715f1604e53b4881f4b02a6
tracer-ysyx <tracer@ysyx.org>
2025-04-16 23:11:07 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:11:07 up  3:57,  1 user,  load average: 0.06, 0.30, 0.54

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -14,7 +14,7 @@ LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 #开启批处理模式
-NEMUFLAGS +=-b
+#NEMUFLAGS +=-b
 NEMUFLAGS += -e $(IMAGE).elf
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = The insert-arg rule in Makefile will insert mainargs here.

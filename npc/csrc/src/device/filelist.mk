a68e483458705f3f9c2ab71bc33178e395246da8
tracer-ysyx <tracer@ysyx.org>
2025-04-25 23:18:32 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:18:32 up 14:52,  1 user,  load average: 0.22, 0.08, 0.03

--- npc/csrc/src/device/filelist.mk
+++ npc/csrc/src/device/filelist.mk
@@ -14,16 +14,16 @@
 #**************************************************************************************/
 
 DIRS-y += src/device/io
-SRCS-$(CONFIG_DEVICE) += src/device/device.c src/device/alarm.c src/device/intr.c
-SRCS-$(CONFIG_HAS_SERIAL) += src/device/serial.c
-SRCS-$(CONFIG_HAS_TIMER) += src/device/timer.c
-SRCS-$(CONFIG_HAS_KEYBOARD) += src/device/keyboard.c
-SRCS-$(CONFIG_HAS_VGA) += src/device/vga.c
-SRCS-$(CONFIG_HAS_AUDIO) += src/device/audio.c
-SRCS-$(CONFIG_HAS_DISK) += src/device/disk.c
-SRCS-$(CONFIG_HAS_SDCARD) += src/device/sdcard.c
+SRCS-$(CONFIG_DEVICE) += csrc/device/device.c csrc/device/alarm.c csrc/device/intr.c
+SRCS-$(CONFIG_HAS_SERIAL) += csrc/device/serial.c
+SRCS-$(CONFIG_HAS_TIMER) += csrc/device/timer.c
+SRCS-$(CONFIG_HAS_KEYBOARD) += csrc/device/keyboard.c
+SRCS-$(CONFIG_HAS_VGA) += csrc/device/vga.c
+SRCS-$(CONFIG_HAS_AUDIO) += csrc/device/audio.c
+SRCS-$(CONFIG_HAS_DISK) += csrc/device/disk.c
+SRCS-$(CONFIG_HAS_SDCARD) += csrc/device/sdcard.c
 
-SRCS-BLACKLIST-$(CONFIG_TARGET_AM) += src/device/alarm.c
+SRCS-BLACKLIST-$(CONFIG_TARGET_AM) += csrc/device/alarm.c
 
 ifdef CONFIG_DEVICE
 ifndef CONFIG_TARGET_AM

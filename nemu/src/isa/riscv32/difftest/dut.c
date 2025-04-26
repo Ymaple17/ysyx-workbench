1495f941511bcc9959839cb9d415a6bf15ec0d7b
tracer-ysyx <tracer@ysyx.org>
2025-04-26 14:02:07 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:02:07 up 18:15,  1 user,  load average: 1.17, 0.64, 0.53

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -16,24 +16,45 @@
 #include <isa.h>
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
-
+/*
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   // Compare the registers one by one
   for (int i = 0; i < 32; ++i) {
     if (cpu.gpr[i] != ref_r->gpr[i]) {
       // If any register value is different, print the mismatch
-      printf("[difftest]Register mismatch at x%d: DUT=%d REF=%d\n", i, cpu.gpr[i], ref_r->gpr[i]);
+      printf("[difftest]  Register mismatch at %s: DUT=%d REF=%d\n", regs[i], cpu.gpr[i], ref_r->gpr[i]);
       return false;
     }
   }
 
   // Compare the program counter (pc)
   if (cpu.pc != ref_r->pc) {
-    printf("[difftest]PC mismatch: DUT=%d REF=%d\n", cpu.pc, ref_r->pc);
+    printf("[difftest]  PC mismatch: DUT=%d REF=%d\n", cpu.pc, ref_r->pc);
     return false;
   }
 
   // If all checks pass, the registers are consistent
+  return true;
+}*/
+
+bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
+  if(cpu.pc != ref_r -> pc)
+  {
+    printf("PC = 0x%x, Difftest failed at PC, Difftest get 0x%x, NEMU get 0x%x\n", cpu.pc, ref_r -> pc, cpu.pc);
+    return false;
+  }
+
+  for(int integer_register_index = 0; integer_register_index < 32; integer_register_index = integer_register_index + 1)
+  {
+    if(cpu.gpr[integer_register_index] != ref_r -> gpr[integer_register_index])
+    {
+      printf("PC = 0x%x, Difftest Reg Compare failed at GPR[%d], Difftest Get 0x%x, NEMU Get 0x%x\n", cpu.pc, integer_register_index, ref_r -> gpr[integer_register_index], cpu.gpr[integer_register_index]);
+      return false;
+    }
+}
+
+  // M-State CSR checkings
+  //printf("PC = 0x%lx, Difftest success\n", cpu.pc);
   return true;
 }
 

4c2e0e94489365f450c24faa51183ad8e7376000
tracer-ysyx <tracer@ysyx.org>
2025-04-26 13:44:51 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:44:51 up 17:57,  1 user,  load average: 0.82, 0.42, 0.36

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,13 +18,22 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  if(cpu.pc == ref_r->pc) {
-    for(int i = 0; i < MUXDEF(CONFIG_RVE, 16, 32); i++) {
-      if (gpr(i) != ref_r->gpr[i]) {
-        return false;
-      }
+  // Compare the registers one by one
+  for (int i = 0; i < 32; ++i) {
+    if (cpu.gpr[i] != ref_r->gpr[i]) {
+      // If any register value is different, print the mismatch
+      printf("Register mismatch at x%d: DUT=%d REF=%d\n", i, cpu.gpr[i], ref_r->gpr[i]);
+      return false;
     }
   }
+
+  // Compare the program counter (pc)
+  if (cpu.pc != ref_r->pc) {
+    printf("PC mismatch: DUT=%d REF=%d\n", cpu.pc, ref_r->pc);
+    return false;
+  }
+
+  // If all checks pass, the registers are consistent
   return true;
 }
 

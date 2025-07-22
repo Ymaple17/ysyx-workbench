485cf6a5ca552fe73f2d9cd9a95231545a346428
tracer-ysyx <tracer@ysyx.org>
2025-07-22 14:21:29 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:21:29 up  2:45,  1 user,  load average: 2.33, 1.69, 1.23

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -26,7 +26,7 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
         memcpy(guest_to_host(addr), buf, n);
     }
     else{
-  assert(0);
+        assert(0);
     }
 }
 

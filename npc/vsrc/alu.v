6d69039f1dcc1b43d3db0ca692c2a08d62e7134b
tracer-ysyx <tracer@ysyx.org>
2025-07-22 09:05:32 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:05:32 up 4 min,  1 user,  load average: 1.36, 0.85, 0.36

--- npc/vsrc/alu.v
+++ npc/vsrc/alu.v
@@ -5,7 +5,12 @@ module ALU (
     input [4:0] ALUFun,
     output reg [`DATA_WIDTH-1:0] Result
 );
+    reg [63:0] mul_temp;
+    
     always @(*) begin
+        mul_temp = 64'h0;
+        Result = {`DATA_WIDTH{1'b0}};
+        
         case (ALUFun)
             5'b00000: Result = A + B;                  // ADD
             5'b00001: Result = A - B;                  // SUB
@@ -18,7 +23,48 @@ module ALU (
             5'b01000: Result = A >> (B & 32'h1f);      // SRL
             5'b01001: Result = $signed(A) >>> (B & 32'h1f); // SRA
             5'b01010: Result = A;                      // Pass A (LUI)
-            default:  Result = 0;
+            5'b01011: Result = A * B;                  // MUL (低32位)
+            5'b01100: begin                            // MULH (有符号乘法高位)
+                mul_temp = $signed(A) * $signed(B);
+                Result = mul_temp[63:32];
+            end
+            5'b01101: begin                            // MULHU (无符号乘法高位)
+                mul_temp = {1'b0, A} * {1'b0, B};
+                Result = mul_temp[63:32];
+            end
+            
+            // 除法指令
+            5'b01110: begin                            // DIV (有符号除法)
+                if (B == {`DATA_WIDTH{1'b0}}) begin
+                    Result = {`DATA_WIDTH{1'b1}};
+                end else begin
+                    Result = $signed(A) / $signed(B);
+                end
+            end
+            5'b01111: begin                            // DIVU (无符号除法)
+                if (B == {`DATA_WIDTH{1'b0}}) begin
+                    Result = {`DATA_WIDTH{1'b1}};
+                end else begin
+                    Result = A / B;
+                end
+            end
+            5'b10000: begin                            // REM (有符号余数)
+                if (B == {`DATA_WIDTH{1'b0}}) begin
+                    Result = A;
+                end else begin
+                    Result = $signed(A) % $signed(B);
+                end
+            end
+            5'b10001: begin                          
+                if (B == {`DATA_WIDTH{1'b0}}) begin
+                    Result = A;
+                end else begin
+                    Result = A % B;
+                end
+            end
+            
+            default:  Result = {`DATA_WIDTH{1'b0}};
         endcase
     end
 endmodule
+    

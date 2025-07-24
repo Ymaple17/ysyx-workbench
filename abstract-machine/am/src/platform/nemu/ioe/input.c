ecd61660f407fa0f1a8e04edc9e9e4f9f7d308fb
tracer-ysyx <tracer@ysyx.org>
2025-07-24 11:25:38 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:25:38 up 1 day,  2:43,  1 user,  load average: 1.25, 0.77, 0.54

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,7 +4,8 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t keycode = inl(KBD_ADDR);
-  kbd->keydown = (keycode & KEYDOWN_MASK ? true : false);
-  kbd->keycode = keycode & ~KEYDOWN_MASK;
+  uint32_t kc = inl(KBD_ADDR);
+  kbd->keydown = kc & KEYDOWN_MASK ? true : false;
+  kbd->keycode = kc & ~KEYDOWN_MASK;
 }
+

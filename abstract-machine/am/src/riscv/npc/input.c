c0fbac58c0848d197de6b379fede6e96379cfe29
tracer-ysyx <tracer@ysyx.org>
2025-07-24 20:01:44 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:01:44 up  3:37,  1 user,  load average: 1.07, 0.79, 0.89

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -1,6 +1,7 @@
 #include <am.h>
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t kbd_value = inl(KBD_ADDR);
+  kbd->keydown = (kbd_value & KEYDOWN_MASK) ? true : false;
+  kbd->keycode = kbd_value & ~KEYDOWN_MASK;
 }

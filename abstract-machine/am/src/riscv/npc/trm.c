32ec46758117c51c5173ec5d71fa07b2a111d7c7
tracer-ysyx <tracer@ysyx.org>
2025-07-24 19:04:43 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:04:43 up  2:40,  1 user,  load average: 2.19, 1.16, 0.86

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,6 +1,5 @@
 #include <am.h>
 #include <klib-macros.h>
-#include <string.h>
 
 # define npc_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
 
@@ -12,7 +11,7 @@ extern char _pmem_start;
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
-static char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
+static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 void putch(char ch) {
 }
@@ -23,10 +22,6 @@ void halt(int code) {
 }
 
 void _trm_init() {
-  const char *actual_args = mainargs;
-  if (strcmp(mainargs, MAINARGS_PLACEHOLDER) == 0) {
-      actual_args = "";
-  }
-  int ret = main(actual_args);
+  int ret = main(mainargs);
   halt(ret);
 }

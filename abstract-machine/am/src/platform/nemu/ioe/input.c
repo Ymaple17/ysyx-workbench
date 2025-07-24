b7d3860b841ab959b1a901bfea935a54c6cbc0f7
tracer-ysyx <tracer@ysyx.org>
2025-07-24 20:14:15 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:14:15 up  3:50,  1 user,  load average: 1.95, 1.27, 0.95

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,8 +4,8 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t kc = inl(KBD_ADDR);
-  kbd->keydown = kc & KEYDOWN_MASK ? true : false;
-  kbd->keycode = kc & KEYDOWN_MASK;
+  uint32_t kbd_value = inl(KBD_ADDR);
+  kbd->keydown = (kbd_value & KEYDOWN_MASK) ? true : false;
+  kbd->keycode = kbd_value & ~KEYDOWN_MASK;
 }
 
--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -1,7 +1,5 @@
 #include <am.h>
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t kbd_value = inl(KBD_ADDR);
-  kbd->keydown = (kbd_value & KEYDOWN_MASK) ? true : false;
-  kbd->keycode = kbd_value & ~KEYDOWN_MASK;
+    // TODO: Implement this function
 }

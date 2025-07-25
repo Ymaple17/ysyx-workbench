d87b292838c8a9ea173340c98957162760fe4775
tracer-ysyx <tracer@ysyx.org>
2025-07-25 10:24:02 +0800

    > compile NEMU ysyx_100012487 余俊源 Linux qiu-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:24:02 up  5:03,  1 user,  load average: 1.04, 0.81, 0.79

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -18,15 +18,12 @@ NPCFLAGS  := -l $(shell dirname $(IMAGE).elf)/npc-log.txt
 NPCFLAGS  += -e $(IMAGE).elf
 NPCFLAGS  += --diff=$(NPC_HOME)/build/riscv32-nemu-interpreter-so
 # NPCFLAGS += -b  # 批处理模式
-
 MAINARGS_MAX_LEN = 64
-MAINARGS_PLACEHOLDER = The insert-arg rule in Makefile will insert mainargs here.
-CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=\""$(MAINARGS_PLACEHOLDER)"\"
-
-.PHONY: image insert-arg run gdb
+MAINARGS_PLACEHOLDER = the_insert-arg_rule_in_Makefile_will_insert_mainargs_here
+CFLAGS += -DMAINARGS_MAX_LEN=$(MAINARGS_MAX_LEN) -DMAINARGS_PLACEHOLDER=$(MAINARGS_PLACEHOLDER)
 
 insert-arg: image
-	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) "$(MAINARGS_PLACEHOLDER)" "$(mainargs)"
+	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
 
 image: image-dep
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
@@ -35,11 +32,14 @@ image: image-dep
 
 run: insert-arg
 	$(MAKE) -C $(NPC_HOME) run \
+		ARGS="$(NPCFLAGS)" \
 		PROGRAM="$(abspath $(IMAGE).bin)" \
-		ARGS="$(NPCFLAGS)"
+		
 
 step: insert-arg
 	$(MAKE) -C $(NPC_HOME) step \
 		PROGRAM="$(abspath $(IMAGE).bin)" \
 		RUN_MODE=step \
 		ARGS="$(NPCFLAGS)"
+
+.PHONY: image insert-arg run gdb

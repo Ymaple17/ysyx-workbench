fd770fb4836d12437cedb9b35adf489acce8419e
tracer-ysyx <tracer@ysyx.org>
2025-07-26 19:12:29 +0800

    > compile NEMU ysyx_25020039 余俊源 Linux qiu-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:12:29 up  5:04,  1 user,  load average: 1.55, 1.29, 1.15

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,9 +7,18 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    switch (c->GPR1) {
-      case 11: ev.event = EVENT_YIELD;c->mepc+=4;break;
-      default: ev.event = EVENT_ERROR; break;
+    if(c -> mcause == 11) {
+      if(c -> GPR1 == -1){
+        ev.event  = EVENT_YIELD;
+        c -> mepc = c -> mepc + 4;
+      }
+      else if(c -> GPR1 >= 0 && c -> GPR1 <= 19){
+        ev.event  = EVENT_SYSCALL;
+        c -> mepc = c -> mepc + 4;
+      }
+      else{
+        ev.event = EVENT_ERROR;
+      }
     }
     c = user_handler(ev, c);
     assert(c != NULL);
@@ -38,7 +47,11 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 }
 
 void yield() {
-  asm volatile("li a7, 11; ecall");
+#ifdef __riscv_e
+  asm volatile("li a5, -1; ecall");
+#else
+  asm volatile("li a7, -1; ecall");
+#endif
 }
 
 bool ienabled() {

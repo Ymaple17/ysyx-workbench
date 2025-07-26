876ff0af0e7859152e37464936191aa5c9295b87
tracer-ysyx <tracer@ysyx.org>
2025-07-26 12:36:52 +0800

    > compile NEMU ysyx_25020039 余俊源 Linux qiu-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:36:52 up  9:49,  1 user,  load average: 1.56, 0.89, 0.77

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -9,7 +9,7 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       case 0: ev.event = EVENT_SYSCALL;break;
-      case 11:  ev.event = EVENT_YIELD;c->mepc += 4;break; 
+      case -1:  ev.event = EVENT_YIELD;c->mepc += 4;break; 
       default: ev.event = EVENT_ERROR; break;
     }
 
@@ -40,7 +40,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 
 void yield() {
 #ifdef __riscv_e
-  asm volatile("li a5, 11; ecall");
+  asm volatile("li a5, -1; ecall");
 #else
   asm volatile("li a7, 11; ecall");
 #endif

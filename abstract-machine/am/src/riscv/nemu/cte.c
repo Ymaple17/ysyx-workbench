2428774ffacced90d3451d774f8d9f0daf63b100
tracer-ysyx <tracer@ysyx.org>
2025-07-26 13:52:04 +0800

    > compile NEMU ysyx_25020039 余俊源 Linux qiu-virtual-machine 6.8.0-64-generic #67~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jun 24 15:19:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:52:04 up 11:04,  1 user,  load average: 0.90, 0.51, 0.65

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -41,11 +41,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 }
 
 void yield() {
-#ifdef __riscv_e
-  asm volatile("li a5, -1; ecall");
-#else
   asm volatile("li a7, 11; ecall");
-#endif
 }
 
 bool ienabled() {
